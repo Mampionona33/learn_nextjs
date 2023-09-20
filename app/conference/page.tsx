44b1afbed0096e25fa08284dee70f077f4e81ff2
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-20 08:12:13 +0000

    Update page.tsx

--- app/conference/page.tsx
+++ app/conference/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Conference() {
   return (
     <>
       <h1>Welcome to Conference Page</h1>
+      <ul>
+        <li>
+          <Link href={"/conference/speakers"}>Speakers</Link>
+        </li>
+        <li>
+          <Link href={"/conference/sessions"}>Sessions</Link>
+        </li>
+      </ul>
     </>
   );
 }
