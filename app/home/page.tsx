e2d0b5113f4a1a8f39a89c5bf12d35c52a6b1586
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-20 07:50:31 +0000

    Update 3 files

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Welcome to Home Page</h1>
+      <ul>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/conference"}>Conference</Link>
+        </li>
+        <li>
+          <Link href={"/settings"}>Settings</Link>
+        </li>
+      </ul>
     </>
   );
 }
