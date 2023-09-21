6b789d19d978bd554d2d99ec3a18f68b94810dff
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-21 13:16:54 +0000

    Update 6 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,40 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  session,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ul className="nav">
-          <li>
-            <Link href={"/home"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/blog"}>Blog</Link>
-          </li>
-          <li>
-            <Link href={"/conference"}>Conference</Link>
-          </li>
-          <li>
-            <Link href={"/settings"}>Settings</Link>
-          </li>
-          <li>
-            <Link href={"/products"}>Products</Link>
-          </li>
-        </ul>
-        {children}
-      </body>
-    </html>
+    <SessionProvider session={session}>
+      <html lang="en">
+        <body className={inter.className}>
+          <ul className="nav">
+            <li>
+              <Link href={"/home"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+            <li>
+              <Link href={"/conference"}>Conference</Link>
+            </li>
+            <li>
+              <Link href={"/settings"}>Settings</Link>
+            </li>
+            <li>
+              <Link href={"/products"}>Products</Link>
+            </li>
+          </ul>
+          {children}
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
