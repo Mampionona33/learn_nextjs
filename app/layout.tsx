68601d57e1ba2fe203010d43a90783906aebee54
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-22 11:05:51 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,36 +13,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SessionProvider session={session}>
-      <html lang="en">
-        <body className={inter.className}>
-          <ul className="nav">
-            <li>
-              <Link href={"/home"}>Home</Link>
-            </li>
-            <li>
-              <Link href={"/blog"}>Blog</Link>
-            </li>
-            <li>
-              <Link href={"/conference"}>Conference</Link>
-            </li>
-            <li>
-              <Link href={"/settings"}>Settings</Link>
-            </li>
-            <li>
-              <Link href={"/products"}>Products</Link>
-            </li>
-            <li>
-              <Link href={"/products"}>singIn</Link>
-            </li>
-            <li>
-              <Link href={"/products"}>singOut</Link>
-            </li>
-          </ul>
+    <html lang="en">
+      <body className={inter.className}>
+        <ul className="nav">
+          <li>
+            <Link href={"/home"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/blog"}>Blog</Link>
+          </li>
+          <li>
+            <Link href={"/conference"}>Conference</Link>
+          </li>
+          <li>
+            <Link href={"/settings"}>Settings</Link>
+          </li>
+          <li>
+            <Link href={"/products"}>Products</Link>
+          </li>
+          <li>
+            <Link href={"/products"}>singIn</Link>
+          </li>
+          <li>
+            <Link href={"/products"}>singOut</Link>
+          </li>
+        </ul>
 
-          {children}
-        </body>
-      </html>
-    </SessionProvider>
+        {children}
+      </body>
+    </html>
   );
 }
